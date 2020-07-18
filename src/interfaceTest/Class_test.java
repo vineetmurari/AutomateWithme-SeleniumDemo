e902f94fbf8cf929aaebbce56c26808a4b7495bd
vineetmurari <68270056+vineetmurari@users.noreply.github.com>
2020-07-18 17:35:18 +0530

    interfaces Demo Test

--- src/interfaceTest/Class_test.java
+++ src/interfaceTest/Class_test.java
@@ -19,8 +19,7 @@ public class Class_test implements Interface1 {
 		System.out.println("test2");
 	}
 	public static void main(String[] args){
-	
-		Interface1 obj = new Class_test();
+		Interface1 obj= new Class_test();
 		obj.test1();
 		obj.test2();
 		obj.test3();
